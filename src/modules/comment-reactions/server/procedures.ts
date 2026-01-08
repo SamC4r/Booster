73f1c7d587c8d8933d4b95e60b920a6300a13797
BreadCat <BreadCat>
2026-01-08 17:51:53 +0100

    Authentication for reaction deletion

--- src/modules/comment-reactions/server/procedures.ts
+++ src/modules/comment-reactions/server/procedures.ts
@@ -2,63 +2,66 @@ import { db } from "@/db";
 import { users, commentReactions, } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import {  inArray } from "drizzle-orm";
+import { inArray, and, eq } from "drizzle-orm";
 import z from "zod";
 
 export const commentReactionsRouter = createTRPCRouter({
     create: protectedProcedure
-    .input(z.object({
-        commentId: z.string().uuid(),
-    }))
-    .mutation(async ({ctx,input}) => {
-        const {clerkUserId} = ctx;
-        const {commentId} = input;
-        
-        // console.log(commentId)
-        const [user]  = await db
-        .select()
-        .from(users)
-        .where(inArray(users.clerkId,clerkUserId ? [clerkUserId] : []))
+        .input(z.object({
+            commentId: z.string().uuid(),
+        }))
+        .mutation(async ({ ctx, input }) => {
+            const { clerkUserId } = ctx;
+            const { commentId } = input;
 
-        if(!user){
-            throw new TRPCError({code:"UNAUTHORIZED"})
-        }
+            // console.log(commentId)
+            const [user] = await db
+                .select()
+                .from(users)
+                .where(inArray(users.clerkId, clerkUserId ? [clerkUserId] : []))
 
-        const userId = user.id;
-       
-        const commentReaction = await db
-        .insert(commentReactions)
-        .values({
-            commentId,
-            userId,
-        }).returning();
-        return commentReaction
-    }),
+            if (!user) {
+                throw new TRPCError({ code: "UNAUTHORIZED" })
+            }
+
+            const userId = user.id;
+
+            const commentReaction = await db
+                .insert(commentReactions)
+                .values({
+                    commentId,
+                    userId,
+                }).returning();
+            return commentReaction
+        }),
 
     delete: protectedProcedure
-    .input(z.object({
-        commentId: z.string().uuid(),
-    }))
-    .mutation(async ({ctx,input}) => {
+        .input(z.object({
+            commentId: z.string().uuid(),
+        }))
+        .mutation(async ({ ctx, input }) => {
 
-        const {clerkUserId} = ctx;
-        const {commentId} = input;
-        
-        const [user]  = await db
-        .select()
-        .from(users)
-        .where(inArray(users.clerkId,clerkUserId ? [clerkUserId] : []))
+            const { clerkUserId } = ctx;
+            const { commentId } = input;
 
-        if(!user){
-            throw new TRPCError({code:"UNAUTHORIZED"})
-        }
+            const [user] = await db
+                .select()
+                .from(users)
+                .where(inArray(users.clerkId, clerkUserId ? [clerkUserId] : []))
+
+            if (!user) {
+                throw new TRPCError({ code: "UNAUTHORIZED" })
+            }
 
 
-        const commentReaction = await db
-        .delete(commentReactions)
-        .where(inArray(commentReactions.commentId,commentId ? [commentId] : [])).returning()
+            const commentReaction = await db
+                .delete(commentReactions)
+                .where(and(
+                    eq(commentReactions.commentId, commentId),
+                    eq(commentReactions.userId, user.id)
+                )).returning()
 
-        return commentReaction
-    })
+            return commentReaction
+        })
 
 })
